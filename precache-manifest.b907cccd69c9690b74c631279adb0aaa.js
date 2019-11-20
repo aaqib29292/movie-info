760f9ceac6fbf4f6ea3f8050e2f29f4723a78f30
Aaqib Parvez <aaqib29292@gmail.com>
2019-11-20 19:42:16 +0530

    Updates

---
+++ precache-manifest.b907cccd69c9690b74c631279adb0aaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a87ca014a8fa7f59aac95f9a7d02a9c",
+    "url": "/movie-info/index.html"
+  },
+  {
+    "revision": "fdce462a4078e0e9c616",
+    "url": "/movie-info/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "3814a0c4786f35e0c772",
+    "url": "/movie-info/static/css/main.bf684d79.chunk.css"
+  },
+  {
+    "revision": "fdce462a4078e0e9c616",
+    "url": "/movie-info/static/js/2.e6de5727.chunk.js"
+  },
+  {
+    "revision": "3814a0c4786f35e0c772",
+    "url": "/movie-info/static/js/main.a0632196.chunk.js"
+  },
+  {
+    "revision": "7e08b8f6c4db801d8128",
+    "url": "/movie-info/static/js/runtime-main.33cd4702.js"
+  }
+]);
